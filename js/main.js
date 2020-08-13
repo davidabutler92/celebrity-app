b14ed034d7a4b34140e3a5feb1bb9eda06456d6f
davidabutler92 <davidabutler92@yahoo.com>
2020-08-13 13:28:41 -0700

    add pull value from name, age and question1 using .val() method

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  
+  $("#survey").submit(function(event) {
+    event.preventDefault();
+
+    const name = $("#name").val();
+    // console.log(name);
+    const age = $("#age").val();
+    const que1 = parseInt($("#question1").val());
+    // console.log(que1);
+  });
 });
